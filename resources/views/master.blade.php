4dffebd3eab1ea2335b3df4a315f4c3194e986fe
Damian Barszcz <barszcz.investments@gmail.com>
2021-11-23 17:15:57 +0100

    Add GTM (Google Tag Manager)

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,8 +8,12 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T4TDZHG"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @yield('basic-root') @yield('auth-root') @yield('member-root') 
-
+    
     <!--  Scripts  -->
     {!! Minify::javascript(['js/app.js']) !!}
 </body>
